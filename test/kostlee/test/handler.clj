8cdcf78086048bd9b4dbf1e5d0b1cf173e7692ce
Pål Ruud <ruudud@gmail.com>
2014-03-24 00:09:52 +0100

    Remove trailing white space

--- test/kostlee/test/handler.clj
+++ test/kostlee/test/handler.clj
@@ -1,13 +1,13 @@
 (ns kostlee.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         kostlee.handler))
 
 (deftest test-app
   (testing "index"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))))
-  
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
